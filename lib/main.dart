93b13b3a06859b6bd01cc1a0695fcb8ab0221a67
Shanmuga Prasath C <22csec56@gmail.com>
2025-07-18 21:56:48 +0530

    update login

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
@@ -23,4 +24,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-"// test change" 
+
